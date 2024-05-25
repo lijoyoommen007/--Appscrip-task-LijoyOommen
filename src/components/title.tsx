0f6ca0b4549e16e0d2b0a3066258e17268401fbc
lijoyoommen007 <108475172+lijoyoommen007@users.noreply.github.com>
2024-05-26 03:16:09 +0530

    logo issue on server

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Title() {
   return (
-    <div className='mt-10 bg-white text-[#292D32]  text-center  flex justify-center align-middle mb-8 '>
+    <div className='mt-4 md:mt-10 bg-white text-[#292D32]  text-center  flex justify-center align-middle mb-8 '>
         <div className='w-[38rem] p-4'>
       <h1 className='md:text-5xl text-3xl'>DISCOVER OUR PRODUCTS</h1>
       <p className='text-xl'>Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.</p>
